8447d7c437625cccaa0d8aa32b280f2fa4367f19
Lasercar <B_l_u_t_a_c_k@hotmail.com.au>
2025-02-07 20:27:58 +1000

    Resetting the instrument music track?

--- source/funkin/ui/story/StoryMenuState.hx
+++ source/funkin/ui/story/StoryMenuState.hx
@@ -658,7 +658,7 @@ class StoryMenuState extends MusicBeatState
     tracklistText.x -= FlxG.width * 0.35;
 
     var levelScore:Null<SaveScoreData> = Save.instance.getLevelScore(currentLevelId, currentDifficultyId);
-    highScore = levelScore?.score ?? 0;
+    highScore = levelScore?.score ?? 69;
     // levelScore.accuracy
   }
 }
