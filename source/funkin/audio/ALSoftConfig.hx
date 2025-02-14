9f1564558c5ab43765db5b516a23b6275cc91dae
cyn <cyn0x8+git@gmail.com>
2025-02-13 23:54:45 -0800

    no hl

--- source/funkin/audio/ALSoftConfig.hx
+++ source/funkin/audio/ALSoftConfig.hx
@@ -14,7 +14,7 @@ class ALSoftConfig
   #if desktop
   static function __init__():Void
   {
-    var origin:String = #if hl Sys.getCwd() #else Sys.programPath() #end;
+    var origin:String = Sys.programPath();
 
     var configPath:String = Path.directory(Path.withoutExtension(origin));
     #if windows
